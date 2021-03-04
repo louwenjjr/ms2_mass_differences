bcddbe3d6c199fdb56212659b0c83538a9610543
Joris Louwen <joris.louwen@wur.nl>
2021-03-04 10:09:21 +0100

    reformatting

--- scripts/jaccard_multiprocessing.py
+++ scripts/jaccard_multiprocessing.py
@@ -2,12 +2,15 @@
 """
 Author: Joris Louwen
 
-Loads data from notebook 2 and calculates jaccard similarity between mass differences and fragments/neutral losses.
+Loads data from notebook 2 and calculates jaccard similarity between mass
+differences and fragments/neutral losses.
 The resulting matrix will be written to a file in output folder.
 
 The files from notebook 2 are called:
-gnps_positive_ionmode_cleaned_by_matchms_and_lookups_mass_difference_occurrence.pickle
-gnps_positive_ionmode_cleaned_by_matchms_and_lookups_fragments_occurrences.pickle
+"gnps_positive_ionmode_cleaned_by_matchms_and_lookups_mass_difference_
+occurrence.pickle"
+"gnps_positive_ionmode_cleaned_by_matchms_and_lookups_fragments_
+occurrences.pickle"
 """
 
 import pickle
@@ -20,20 +23,25 @@ from functools import partial
 from math import ceil
 
 
-def get_commands():
-    """Returns argparse.ArgumentParser.parse_args object for command line options
+def get_commands() -> argparse.Namespace:
     """
-    parser = argparse.ArgumentParser(description="Loads data from notebook 2 in this repo\
-        and calculates jaccard similarity between mass differences and fragments/neutral\
-        losses in a multithreading way. Resulting matrix will be written to output_file.")
-    parser.add_argument("-m", "--mds", metavar="<.pickle>", help="pickle file containing\
-        list of list of mass difference occurrences (tuple)", required=True)
-    parser.add_argument("-f", "--fragments", metavar="<.pickle>", help="pickle file containing\
-        list of list of fragment/neutral loss occurrences (tuple)", required=True)
-    parser.add_argument("-o", "--output_file", metavar="<file>", help="location of output\
-        file (default: jaccard_matrix.csv)", default="jaccard_matrix.csv")
-    parser.add_argument("-c", "--cores", help="Cores to use (default: 20)", default=20,
-        type=int)
+    Returns argparse.ArgumentParser.parse_args object for command line options
+    """
+    parser = argparse.ArgumentParser(description="Loads data from notebook 2\
+        in this repo and calculates jaccard similarity between mass\
+        differences and fragments/neutral losses in a multithreading way.\
+        Resulting matrix will be written to output_file.")
+    parser.add_argument("-m", "--mds", metavar="<.pickle>", help="pickle file\
+        containing list of list of mass difference occurrences (tuple)",
+                        required=True)
+    parser.add_argument("-f", "--fragments", metavar="<.pickle>", help="pickle\
+        file containing list of list of fragment/neutral loss occurrences\
+        (tuple)", required=True)
+    parser.add_argument("-o", "--output_file", metavar="<file>",
+                        help="location of output file (default:\
+                        jaccard_matrix.csv)", default="jaccard_matrix.csv")
+    parser.add_argument("-c", "--cores", help="Cores to use (default: 20)",
+                        default=20, type=int)
     return parser.parse_args()
 
 
@@ -54,25 +62,31 @@ def jaccard_list_occurrences(list_1: List[str], list_2: List[str]) -> float:
 
 
 def calculate_row_jaccard(md_occ_list: List[str],
-                          all_fragment_occ_list: List[List[str]]) -> List[Union[str,float]]:
-    """For one mass difference, calc Jaccard similarity to all fragments/neutral losses
+                          all_fragment_occ_list: List[List[str]]) \
+        -> List[Union[str, float]]:
+    """
+    For one mass difference, calc Jaccard similarity to all fragments/n_losses
     
     Parameters
     -----------
     md_occ_list:
-        List of [MD_name, spectra names] in which a MD occurs, first element is MD name
+        List of [MD_name, spectra names] in which a MD occurs, first element
+        is MD name
     all_fragment_occ_list:
-        List of list of spectra names of spectra occurrences for all fragments/neutral losses
+        List of list of spectra names of spectra occurrences for all fragments/
+        neutral losses
 
     Returns
     -----------
     jaccard_sims:
-        List of [MD_name, jaccard_similarities], first element is str, rest are floats
+        List of [MD_name, jaccard_similarities], first element is str, rest are
+        floats
     """
     md_name = md_occ_list.pop(0)
     jaccard_sims = [md_name]
     for frag_occ_list in all_fragment_occ_list:
-        jaccard_sims.append(jaccard_list_occurrences(md_occ_list, frag_occ_list))
+        jaccard_sims.append(
+            jaccard_list_occurrences(md_occ_list, frag_occ_list))
     return jaccard_sims
 
 
@@ -91,7 +105,7 @@ def main():
         fragment_occ = pickle.load(inf)
 
     # get only the occurrences
-    just_md_occ = [[tup[0]]+tup[1] for tup in md_occ]
+    just_md_occ = [[tup[0]] + tup[1] for tup in md_occ]
     just_fragment_occ = [tup[1] for tup in fragment_occ]
 
     # create/overwrite output file with header
@@ -101,13 +115,17 @@ def main():
     # calc jaccard with multiprocessing, in chuncks of 10,000 rows
     print("\nStart with calculations")
     chunk_len = 7500
-    num_chunks = ceil(len(just_md_occ)/chunk_len)
+    num_chunks = ceil(len(just_md_occ) / chunk_len)
     for chunk_num in range(num_chunks):
-        print(f"\nData chunk number {chunk_num}/{num_chunks-1}")
-        current_chunk = just_md_occ[chunk_len*chunk_num: chunk_len*(chunk_num+1)]
+        print(f"\nData chunk number {chunk_num}/{num_chunks - 1}")
+        current_chunk = just_md_occ[
+                        chunk_len * chunk_num: chunk_len * (chunk_num + 1)]
         pool = Pool(processes=cmd.cores)
-        jaccard_sims = pool.imap_unordered(partial(calculate_row_jaccard,
-            all_fragment_occ_list=just_fragment_occ), current_chunk, chunksize=100)
+        jaccard_sims = pool.imap_unordered(
+            partial(
+                calculate_row_jaccard,
+                all_fragment_occ_list=just_fragment_occ),
+            current_chunk, chunksize=100)
         pool.close()
         pool.join()
 
@@ -116,10 +134,11 @@ def main():
         with open(cmd.output_file, 'a') as outf:
             for j_sims in jaccard_sims:
                 outf.write("{}\n".format(",".join(map(str, j_sims))))
-        del(pool, jaccard_sims)
+        del (pool, jaccard_sims)
 
     end = time.time()
-    print("Time elapsed (hours): ", (end-start)/3600)
+    print("Time elapsed (hours): ", (end - start) / 3600)
+
 
 if __name__ == "__main__":
     main()
